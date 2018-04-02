910d09cdd6315cba6774f07b6a8d54f039750778
sandond77 <sandu@ucdavis.edu>
2018-04-01 22:42:10 -0700

    end of ex11

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,17 +1,28 @@
-import React, { Fragment } from "react";
+import React from "react";
+import { getFunName } from "../helpers";
 
 class StorePicker extends React.Component {
+	myInput = React.createRef();
+
+	goToStore = (event) => {
+		event.preventDefault();
+		console.log(this.myInput.value.value)
+	}
+
+
 	render() {
 		return (
-			<Fragment>
-				{/*This is a store picker */}
-				<form action="" className="store-selector">
-					<h2> Please Enter a Store</h2>
-					<input type="text" required placeholder="Store Name" />
-					<button type="submit"> Visit Store →</button>
-				</form>
-				{/*this is a comment*/}
-			</Fragment>
+			<form className="store-selector" onSubmit={this.goToStore}>
+				<h2> Please Enter a Store</h2>
+				<input 
+					type="text" 
+					ref={this.myInput}
+					required 
+					defaultValue={getFunName()}
+					placeholder="Store Name" 
+				/>
+				<button type="submit"> Visit Store →</button>
+			</form>
 		);
 	}
 }
