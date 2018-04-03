659474b3e53dd347423e7aa5ce071e1c7f1e6d04
sandond77 <sandu@ucdavis.edu>
2018-04-02 18:16:05 -0700

    end of ex 14, set state w/ sample fishes

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   state = {
@@ -18,6 +19,12 @@ class App extends React.Component {
     })
   }
 
+  loadSampleFishes = () => {
+    console.log("Loading sample fishes")
+    this.setState({
+      fishes: sampleFishes
+    })
+  }
 
   render() {
     return (
@@ -26,7 +33,10 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory 
+          addFish={this.addFish} 
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
