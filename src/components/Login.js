4910c0943f36cff7dd195b92dec8d9b5aa2bdd30
sandond77 <sandu@ucdavis.edu>
2018-07-12 11:03:04 -0700

    added login props and buttons to login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,31 @@
 import React from 'react'; 
+import PropTypes from 'prop-types';
 
 const Login = () => (
 	<nav className="Login">
 		<h2>Inventory</h2>
+		<p> Sign in to mange your store's inventory </p>
+		<button 
+		className="github" 
+		onclick={( => this.props.authenticate('GitHub')}
+			Log In with Github
+		</button>
+		<button 
+		className="twitter" 
+		onclick={( => this.props.authenticate('Twitter')}
+			Log In with Twitter
+		</button>
+		<button 
+		className="facebook" 
+		onclick={( => this.props.authenticate('Facebook')}
+			Log In with Facebook
+		</button>
+
 	</nav>
 )
+
+Login.propTypes = {
+	authenticate: PropType.func.isRequired;
+};
 
 export default Login;
